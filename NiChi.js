c76739112b55edcac6c25305bf52705a2028b59a
stillfantasy <pengbo302@gmail.com>
2020-04-15 13:32:23 +0800

    Update NiChi.js

--- NiChi.js
+++ NiChi.js
@@ -4,10 +4,10 @@ Because QX limits the maximum rewrite size of the response body, So this script 
 If your QX version is greater than 1.0.1 (130) then it is recommended to point to the local script path and enable AlwaysOn.
 
 Surge4.0:
-http-response https?:\/\/mp\.bybutter\.com\/mood\/(official-templates|privileges) requires-body=1,max-size=524288,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/NiChi.js
+http-response https?:\/\/mp\.bybutter\.com\/mood\/(official-templates|privileges) requires-body=1,max-size=524288,script-path=https://raw.githubusercontent.com/zhupai/Scripts/master/NiChi.js
 
 QX 1.0.0:
-^https?:\/\/mp\.bybutter\.com\/mood\/(official-templates|privileges) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/NiChi.js
+^https?:\/\/mp\.bybutter\.com\/mood\/(official-templates|privileges) url script-response-body https://raw.githubusercontent.com/zhupai/Scripts/master/NiChi.js
 
 Surge & QX Mitm = mp.bybutter.com
 */

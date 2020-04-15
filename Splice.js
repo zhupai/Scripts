10c1cdf23a15dfb87f241cfe2c40e5ee870eb3f2
stillfantasy <pengbo302@gmail.com>
2020-04-15 13:20:15 +0800

    Update Splice.js

--- Splice.js
+++ Splice.js
@@ -2,10 +2,10 @@
 Splice app unlock pro.
 
 Surge4.0:
-http-response https:\/\/splice\.oracle\.\w+\.com\/devices\/me requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Splice.js
+http-response https:\/\/splice\.oracle\.\w+\.com\/devices\/me requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/zhupai/Scripts/master/Splice.js
 
 QX 1.0.0:
-https:\/\/splice\.oracle\.\w+\.com\/devices\/me url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Splice.js
+https:\/\/splice\.oracle\.\w+\.com\/devices\/me url script-response-body https://raw.githubusercontent.com/zhupai/Scripts/master/Splice.js
 
 Surge & QX Mitm = splice.oracle.*.com
 */

f4c2e6ea05dfc76d66d40bc7688c229d5425b145
stillfantasy <pengbo302@gmail.com>
2020-04-15 13:25:11 +0800

    Update PicsArt.js

--- PicsArt.js
+++ PicsArt.js
@@ -2,10 +2,9 @@
 PicsArt app unlocks pro
 
 Surge4.0:
-http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
-
+http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://github.com/zhupai/Scripts/raw/master/PicsArt.js
 QX1.0.0
-^https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+^https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json url script-response-body https://github.com/zhupai/Scripts/raw/master/PicsArt.js
 
 Surge & QX Mitm = api.picsart.c*, api.meiease.c*
 */
